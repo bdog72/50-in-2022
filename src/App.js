7d97ee623c1944d44c7bae26165903e6cac75004
Brian Bycynski <high5bri@hotmail.com>
2022-03-23 01:47:06 -0400

    Init Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+//
+//
+
+import './App.css';
+
+// import { Article, Brand, CTA, Feature, Navbar } from './components/index';
+
+function App() {
+  return (
+    <div className=''>
+      {/* <Article />
+      <Brand />
+      <CTA />
+      <Feature />
+      <Navbar /> */}
+    </div>
+  );
+}
+
+export default App;
