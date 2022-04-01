019dde8cfb91eeb7f481794d336a19d07e6e024f
Brian Bycynski <high5bri@hotmail.com>
2022-04-01 02:18:24 -0400

    Project

--- Projects/8-split-landing-page/script.js
+++ Projects/8-split-landing-page/script.js
@@ -1,4 +1,22 @@
 //
 //
 
-console.log(1111);
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+left.addEventListener('mouseenter', () => {
+  return container.classList.add('hover-left');
+});
+
+left.addEventListener('mouseleave', () => {
+  return container.classList.remove('hover-left');
+});
+
+right.addEventListener('mouseenter', () => {
+  return container.classList.add('hover-right');
+});
+
+right.addEventListener('mouseleave', () => {
+  return container.classList.remove('hover-right');
+});
