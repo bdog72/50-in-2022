f7a75f57c2383369f16d67a02ae51fb175eba587
Brian Bycynski <high5bri@hotmail.com>
2022-04-03 03:52:35 -0400

    project

--- Projects/11-dad-jokes/script.js
+++ Projects/11-dad-jokes/script.js
@@ -1,2 +1,23 @@
 //
 //
+
+const jokeEl = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
+const generateJoke = () => {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+
+  jokeBtn.addEventListener('click', generateJoke);
+
+  fetch('https://icanhazdadjoke.com/', config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke;
+    });
+};
+
+generateJoke();
