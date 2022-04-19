aff25c329628a43ca050e10768fd550b2b2c11a5
Brian Bycynski <high5bri@hotmail.com>
2022-04-19 03:35:11 -0400

    Update script.js

--- Projects/16-drink-water/script.js
+++ Projects/16-drink-water/script.js
@@ -1,4 +1,29 @@
 //
 //
 
-console.log(1111);
+const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
+
+smallCups.forEach((cup, index) => {
+  cup.addEventListener('click', () => highlightCups(index));
+});
+
+function highlightCups(idx) {
+  if (
+    smallCups[idx].classList.contains('full') &&
+    !smallCups[idx].nextElementSibling.classList.contains('full')
+  ) {
+    idx--;
+  }
+
+  smallCups.forEach((cup, index2) => {
+    if (index2 <= idx) {
+      cup.classList.add('full');
+    } else {
+      cup.classList.remove('full');
+    }
+  });
+}
