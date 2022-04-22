67ad7fa0e3454deb7a72a0860f93e37957c14e6b
Brian Bycynski <high5bri@hotmail.com>
2022-04-22 02:56:08 -0400

    Update script.js

--- Projects/17-movie-app/script.js
+++ Projects/17-movie-app/script.js
@@ -1,4 +1,17 @@
 //
 //
 
-console.log(1111);
+const API_URL =
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c9a0e7293dcd7837f1c9ee59610fc8eb&page=1';
+
+console.log(API_URL);
+
+// const API_URL =
+//   'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4a88b73ad6bac59964d776e8a6fb0b84&page=1';
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=c9a0e7293dcd7837f1c9ee59610fc8eb&query="';
+
+console.log(SEARCH_API);
